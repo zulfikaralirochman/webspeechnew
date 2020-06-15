149ca7a96225dd666010693ac0d0f8620706309b
zulfikaralirochman <63213283+zulfikaralirochman@users.noreply.github.com>
2020-06-15 08:27:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.static(__dirname + '/views')); // html
 app.use(express.static(__dirname + '/public')); // js, css, images
 
-const server = app.listen(process.env.PORT || 5000, () => {
+const server = app.listen(process.env.PORT || 8080, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
 
